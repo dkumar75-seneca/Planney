93f93655ad9d0f41261f8e5fb7da47b5180f35f1
dkumar75-seneca <121585403+dkumar75-seneca@users.noreply.github.com>
2023-11-04 14:29:09 -0400

    Update CRUD.js

--- server/database/CRUD.js
+++ server/database/CRUD.js
@@ -13,3 +13,48 @@ function UpdateRecord(collectionName) {
 function DeleteRecord(collectionName) {
   ;
 }
+
+async function insertDocument() {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const collection = client.db().collection('THIS-IS-JUST-A-TEST');
+
+  const documentToInsert = { name: 'John', age: 30, email: 'john@example.com' };
+  const result = await collection.insertOne(documentToInsert);
+  console.log('Inserted document:');
+  client.close();
+}
+
+async function findDocuments() {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const collection = client.db().collection('your_collection_name');
+
+  const query = { age: { $gte: 25 } }; // Find documents where age is greater than or equal to 25
+  const documents = await collection.find(query).toArray();
+  console.log('Found documents:', documents);
+  client.close();
+}
+
+async function updateDocument() {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const collection = client.db().collection('your_collection_name');
+
+  const filter = { name: 'John' };
+  const update = { $set: { age: 31 } }; // Update John's age to 31
+  const result = await collection.updateOne(filter, update);
+  console.log('Updated document:', result);
+  client.close();
+}
+
+async function deleteDocument() {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const collection = client.db().collection('THIS-IS-JUST-A-TEST');
+
+  const filter = { name: 'John' };
+  const result = await collection.deleteOne(filter);
+  console.log('Deleted document:', result);
+  client.close();
+}
