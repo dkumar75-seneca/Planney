f8e7be546ca33442ee186dcda8b40e31b673cfe3
dkumar75-seneca <dkumar75@myseneca.ca>
2023-11-15 15:50:19 -0500

    minor update to server code

--- server/src/requestValidator.js
+++ server/src/requestValidator.js
@@ -4,8 +4,6 @@ const { collectionNames, collectionFields } = require('../database/collectionNam
 
 function ValidateString(input) { return true; }
 function ValidateNumber(input) { return true; }
-function ValidateBoolean(input) { return true; }
-function ValidateDatetime(input) { return true; }
 
 exports.CheckRequest = function(collectionNum, inputData) {
   if (collectionNum < 0 || collectionNum >= collectionNames.length) { return false; }
@@ -15,17 +13,18 @@ exports.CheckRequest = function(collectionNum, inputData) {
     const fieldName = keyFields[i][0], fieldType = keyFields[i][1];
     if (!inputData[fieldName]) { return false; }
     
-    if (fieldType === "string" || fieldType === "number" || fieldType === "boolean") {
+    if (fieldType === "string" || fieldType === "number") {
       let check = false;
       if (!typeof inputData[fieldName] === fieldType) { return false; }
       if (fieldType === "string") { check = ValidateString(inputData[fieldName]); }
       else if (fieldType === "number") { check = ValidateNumber(inputData[fieldName]); }
-      else if (fieldType === "boolean") { check = ValidateBoolean(inputData[fieldName]); }
       if (!check) { return false; }
     } else {
       if (Array.isArray(inputData[fieldName])) {
         if (fieldType === "datetime") {
-          if (!ValidateDatetime(inputData[fieldName])) { return false; }
+          for (let i = 0; i < inputData[fieldName].length; i++) {
+            if (!ValidateNumber(inputData[fieldName][i])) { return false; }
+          }
         } else {
           for (let i = 0; i < inputData[fieldName].length; i++) {
             if (!ValidateString(inputData[fieldName][i])) { return false; }
