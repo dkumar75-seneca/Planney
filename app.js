f414c0d4c3e93dbeaea40e3c262d28cdb1e9d4de
dkumar75-seneca <121585403+dkumar75-seneca@users.noreply.github.com>
2023-12-06 16:46:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./server/router');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: ['https://planneynew.azurewebsites.net/'] }));
 
 app.use(logger('dev'));
 app.use(express.json());
